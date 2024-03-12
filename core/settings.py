68a68806fbcf2a4064b19627173d2a8ca899c2df
Mojtaba Ghasemi <mghasemi13786@gmail.com>
2024-03-12 22:07:55 +0330

    [Feature] add env

--- core/settings.py
+++ core/settings.py
@@ -12,18 +12,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ymu59sg6-=)du%#0x*_g8no9%q3*ihf)i%qz)s!(^ikz!%6*4'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -77,8 +82,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('PG_NAME'),
+        'PORT': env('PG_PORT'),
+        'PASSWORD': env('PG_PASSWORD'),
+        'USER': env('PG_USER'),
+        'HOST': env('PG_HOST'),
     }
 }
 
